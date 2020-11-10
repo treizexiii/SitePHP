ca7d1849d05734910edf79b9fa0aab6586973f6c
Treize <jonathan.rougier@hotmail.fr>
2020-11-10 06:34:08 +0100

    ajout contact

--- contact.php
+++ contact.php
@@ -1,6 +1,50 @@
 <?php $title = "Nous contacter";
+require_once 'config.php';
+require 'function.php';
+
+date_default_timezone_set('Europe/Paris');
+$heure = (int)($_GET['heure'] ?? date('G'));
+$jour = (int)($_GET['jour'] ?? date('N') - 1);
+$creneaux = CRENEAUX[$jour];
+$ouvert = in_creneaux($heure, $creneaux);
+$color = $ouvert ? 'green' : 'red';
+
 require 'header.php'; ?>
 
-<h2>Nous Contacter</h2>
+<div class="row">
+    <div class="clo-md-8">
+        <h2>Nous Contacter</h2>
+        <p>Dans les textes non linéaires, généralement tabulaires, il est difficile de parler de paragraphes : la page est composée de tables ou de tableaux, de graphes et d'histogrammes, d'images (de photographies, de dessins, ou de schémas, etc.), où les informations textuelles figurent dans des pavés de type légende, commentaire, note, etc., chaque segment de texte étant plus ou moins indépendant des autres, et rattaché à un élément non textuel. Il vaut mieux dans ce cas parler de pavé(s), et envisager la composition du document sous l'angle de la topologie (de la mise en page(s)).</p>
+    </div>
+    <div class="col-md-4">
+        <h2>Horraire d'ouverture</h2>
+        <?php if ($ouvert) : ?>
+            <div class="alert alert-success">
+                Le magasin est ouvert
+            </div>
+        <?php else : ?>
+            <div class="alert alert-danger">
+                Le magasin est fermé
+            </div>
+        <?php endif; ?>
+        <form action="" method="GET">
+            <div class="form-group">
+                <?= select('jour', $jour, JOURS) ?>
+            </div>
+            <div class="form-group">
+                <input class="form-control" type="number" name="heure" value="<?= $heure ?>">
+            </div>
+            <button class="btn btn-primary" type="submit">Voir si le magasin est ouvert</button>
+        </form>
+        <ul>
+            <?php foreach (JOURS as $k => $jour) : ?>
+                <li>
+                    <strong><?= $jour ?></strong>
+                    <?= creneaux_html(CRENEAUX[$k]) ?>
+                </li>
+            <?php endforeach; ?>
+        </ul>
+    </div>
+</div>
 
 <?php require 'footer.php'; ?>
--- function.php
+++ function.php
@@ -45,12 +45,44 @@ HTML;
     return $result;
 }
 
+function select(string $name, $value, array $options): string 
+{
+    $html_options = [];
+    foreach($options as $k => $option){
+        $attributes = $k == $value ? 'selected' : '';
+        $html_options[] = "<option value ='$k' $attributes>$option</option>";
+    }
+    return "<select class='form-control' name='$name'>" . implode($html_options) . '</select>';
+}
+
 function dump($variable)
 {
     echo'<pre>';
     var_dump($variable);
     echo '</pre>';
 }
-?>
 
-<?php
+function creneaux_html(array $creneaux){
+    if(empty($creneaux)){
+        return "Fermé.";
+    }
+    $phrases = [];
+    foreach($creneaux as $creneau){
+        $phrases[] = "de <strong>{$creneau[0]}h</strong> à <strong>{$creneau[1]}h</strong>";
+    }
+    return "Ouvert ".implode(' & ', $phrases);
+}
+
+function in_creneaux(int $heure, array $creneaux): bool
+{
+    foreach($creneaux as $creneau){
+        $debut = $creneau[0];
+        $fin = $creneau[1];
+        if($heure>= $debut && $heure < $fin){
+            return true;
+        }
+    }
+    return false;
+}
+
+?>
